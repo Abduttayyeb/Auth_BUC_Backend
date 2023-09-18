b35519d47c429c5a04bf25b5735027f686bc9245
abduttayyeb <absking786@gmail.com>
2023-09-18 18:36:09 +0530

    MongDB Conn | ODM Mongoose

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const { logger } = require("./middleware/logger");
-const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser"); // This middleware simplifies the process of handling cookies
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+connectDB();
 
 app.use(logger);
 app.use(cors(corsOptions));
@@ -17,6 +24,14 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, `/public`)));
 app.use("/", require("./routes/root"));
 
-app.use(errorHandler);
+app.use(errorHandler); // This error-handling middleware function is added at the end of the middleware function stack.
 
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+mongoose.connection.once("open", () => {
+    console.log("Connected to DB");
+    app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log(err);
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, "mongoErrLog.log");
+});
